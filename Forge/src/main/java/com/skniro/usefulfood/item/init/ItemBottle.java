eb1ab64a5d9d7d3650772afb220dcdb0b6aaef15
skniro <46392259+skniro@users.noreply.github.com>
2024-06-19 13:29:12 +0800

    Fix item effect.

--- Forge/src/main/java/com/skniro/usefulfood/item/init/ItemBottle.java
+++ Forge/src/main/java/com/skniro/usefulfood/item/init/ItemBottle.java
@@ -1,14 +1,13 @@
 package com.skniro.usefulfood.item.init;
 
 
-import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import com.skniro.usefulfood.item.UsefulFoodItems;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 public class ItemBottle
@@ -21,7 +20,7 @@ public class ItemBottle
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity user) {
-        if (!world.isClientSide) {
+        if (!world.isClientSide && stack.is(UsefulFoodItems.MilkBottle.get())) {
             user.removeAllEffects();
         }
         ItemStack $$3 = super.finishUsingItem(stack, world, user);

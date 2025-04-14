e1b44451e83ac3f9fba7fc978ece39be98cdc2e8
skniro <46392259+skniro@users.noreply.github.com>
2025-04-14 21:18:14 +0800

    Merged improve ItemBottle functionality.

--- Fabric/src/main/java/com/skniro/usefulfood/item/init/ItemBottle.java
+++ Fabric/src/main/java/com/skniro/usefulfood/item/init/ItemBottle.java
@@ -1,33 +1,51 @@
 package com.skniro.usefulfood.item.init;
 
 import com.skniro.usefulfood.item.UsefulFoodItems;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
 
 public class ItemBottle
         extends Item {
     private static final int MAX_USE_TIME = 32;
 
-    public ItemBottle(Item.Settings settings) {
+    public ItemBottle(Settings settings) {
         super(settings);
     }
 
-
+    @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        if (!world.isClient && stack.isOf(UsefulFoodItems.MilkBottle)) {
-            user.clearStatusEffects();
+        PlayerEntity playerEntity;
+        super.finishUsing(stack, world, user);
+
+        // Advancement check + stat
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
         }
-        ItemStack itemStack = super.finishUsing(stack, world, user);
-        return user instanceof PlayerEntity && ((PlayerEntity)user).getAbilities().creativeMode ? itemStack : new ItemStack(Items.GLASS_BOTTLE);
+
+        // Clear statuses (if milk)
+        if (!world.isClient && stack.isOf(UsefulFoodItems.MilkBottle))
+            user.clearStatusEffects();
+
+
+        if (user instanceof PlayerEntity && !(playerEntity = (PlayerEntity)user).isInCreativeMode()) {
+            ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+            if (!playerEntity.getInventory().insertStack(itemStack)) {
+                playerEntity.dropItem(itemStack, false);
+            }
+        }
+        return stack;
     }
 
     @Override
     public int getMaxUseTime(ItemStack stack, LivingEntity user) {
         return MAX_USE_TIME;
     }
-
 }
--- Neoforge/src/main/java/com/skniro/usefulfood/item/init/ItemBottle.java
+++ Neoforge/src/main/java/com/skniro/usefulfood/item/init/ItemBottle.java
@@ -1,7 +1,9 @@
 package com.skniro.usefulfood.item.init;
 
-
 import com.skniro.usefulfood.item.UsefulFoodItems;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -19,15 +21,31 @@ public class ItemBottle
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity user) {
-        if (!world.isClientSide && stack.is(UsefulFoodItems.MilkBottle.get())) {
-            user.removeAllEffects();
+        Player playerEntity;
+        super.finishUsingItem(stack, world, user);
+
+        // Advancement check + stat
+        if (user instanceof ServerPlayer serverPlayerEntity) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.awardStat(Stats.ITEM_USED.get(this));
         }
-        ItemStack $$3 = super.finishUsingItem(stack, world, user);
-        return user instanceof Player && ((Player)user).getAbilities().instabuild ? $$3 : new ItemStack(Items.GLASS_BOTTLE);
+
+        // Clear statuses (if milk)
+        if (!world.isClientSide && stack.is(UsefulFoodItems.MilkBottle.get()))
+            user.removeAllEffects();
+
+
+        if (user instanceof Player && !(playerEntity = (Player)user).hasInfiniteMaterials()) {
+            ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+            if (!playerEntity.getInventory().add(itemStack)) {
+                playerEntity.drop(itemStack, false);
+            }
+        }
+        return stack;
     }
 
     @Override
     public int getUseDuration(ItemStack stack, LivingEntity user) {
-        return MAX_USE_TIME ;
+        return MAX_USE_TIME;
     }
 }

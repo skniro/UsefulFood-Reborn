8b80ebdbc7d782da60976b414c36cc86f34d40e0
skniro <46392259+skniro@users.noreply.github.com>
2025-07-01 18:48:36 +0800

    1.21.7

--- Neoforge/src/main/java/com/skniro/usefulfood/UsefulFood.java
+++ Neoforge/src/main/java/com/skniro/usefulfood/UsefulFood.java
@@ -49,7 +49,7 @@ public class UsefulFood {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
+    @EventBusSubscriber(modid = MODID)
     public static class ClientModEvents {
 
         @SubscribeEvent
